a57c562f72e70315d0ae471c22149456eb869263
3swordman <yyuxiaoran@qq.com>
2022-04-04 17:56:43 +0800

    1+1=2

--- api/index.py
+++ api/index.py
@@ -11,28 +11,34 @@ app = Flask(__name__)
 rds = redis.StrictRedis(host=config["redis"]["ip"], port=config["redis"]["port"],
                         db=config["redis"]["db"], password=config["redis"]["password"])
 
+def json_route(*args1, **kwargs1):
+    def ret_func(func):
+        @json_route(*args1, **kwargs1)
+        def ret_func2(*args2, **kwargs2):
+            return json.dumps(func(*args2, **kwargs2), ensure_ascii=False)
+        return ret_func2
+    return ret_func
 
-
-@app.route('/api/')
+@json_route('/api/')
 def index():
-    return json.dumps({'status': 'ok', 'message': 'Hello World!'}, ensure_ascii=False)
+    return {'status': 'ok', 'message': 'Hello World!'}
 
 
 #POST /api/comment_s
-@app.route('/api/comment_s', methods=['POST', "GET"])
+@json_route('/api/comment_s', methods=['POST', "GET"])
 def comment_s():
     # 不可维护的代码awa
     content_length=len(request.json['content'])
-    if content_length<1 or content_length>100:
-        return json.dumps({'status': 'error', 'message': 'content length error'}, ensure_ascii=False)
+    if content_length < 1 or content_length > 100:
+        return {'status': 'error', 'message': 'content length error'}
     ip = request.remote_addr
     if ip in config["ip_blacklist"]:
         return {'status': 'error', 'message': 'ip is banned forever'}
-    ipk = "connect_"+ip
-    ipb = "isStop_"+ip
-    ipban = "ip_"+ip
+    ipk = f"connect_{ip}"
+    ipb = f"isStop_{ip}"
+    ipban = f"ip_{ip}"
     if rds.exists(ipban) and int(rds[ipban].decode(encoding="utf-8")) >= 4:
-        return json.dumps({'status': 'error', 'message': '你已经被禁了'}, ensure_ascii=False)
+        return {'status': 'error', 'message': '你已经被禁了'}
     if rds.exists(ipk) and int(rds[ipk].decode(encoding="utf-8")) >= 4:
         if rds.exists(ipban) and not rds.exists(ipb):
             rds.set(ipban, int(rds[ipban].decode(encoding="utf-8"))+1)
@@ -42,7 +48,7 @@ def comment_s():
             rds.expire(ipban, 60*60*24)
         rds.set(ipb,1)
         rds.expire(ipb, 60)
-        return json.dumps({'status': 'error', 'message': '请求过于频繁，请稍后再试'}, ensure_ascii=False)
+        return {'status': 'error', 'message': '请求过于频繁，请稍后再试'}
     elif not rds.exists(ipk):
         rds.set(ipk, '1')
         rds.expire(ipk, 60)
@@ -51,20 +57,20 @@ def comment_s():
         rds.expire(ipk, 60)
     try:
         if request.json.get("comment_type")==None or request.json.get("comment_type")=="":
-            return json.dumps({'status': 'error', 'message': 'type is required'}, ensure_ascii=False)
+            return {'status': 'error', 'message': 'type is required'}
         comment_content = clean(request.json['content'], tags=[
                                 "strong", "em", "mark", "del", "u", "a", "img", "blockquote"], strip=True)
         if request.json["comment_type"]=="normal":
-            cid = "comment_"+uuid.uuid5(uuid.NAMESPACE_DNS,comment_content+str(time.time())).hex
+            cid = f"comment_{uuid.uuid5(uuid.NAMESPACE_DNS,comment_content+str(time.time())).hex}"
         elif request.json["comment_type"]=="destroy":
-            cid = "destroy_"+uuid.uuid5(uuid.NAMESPACE_DNS,comment_content+str(time.time())).hex
+            cid = f"destroy_{uuid.uuid5(uuid.NAMESPACE_DNS,comment_content+str(time.time())).hex}"
         rds[cid] = comment_content
-        return json.dumps({'status': 'ok', 'message': 'Comment Commited', 'id': cid}, ensure_ascii=False)
+        return {'status': 'ok', 'message': 'Comment Commited', 'id': cid}
     except Exception as e:
-        return json.dumps({'status': 'error', 'message': str(e)}, ensure_ascii=False)
+        return {'status': 'error', 'message': str(e)}
 
 
-@app.route('/api/comment_g/<string:id>')
+@json_route('/api/comment_g/<string:id>')
 def get_comment(id):
     id=id.strip()
     id_type=id.split("_")[0]
@@ -72,50 +78,50 @@ def get_comment(id):
         content=rds[id].decode(encoding="utf-8")
         if id_type=="destroy":
             rds.delete(id)
-        return json.dumps({'status': 'ok', 'message': 'Comment found', 'content': content}, ensure_ascii=False)
+        return {'status': 'ok', 'message': 'Comment found', 'content': content}
     else:
-        return json.dumps({'status': 'error', 'message': 'Comment not found'}, ensure_ascii=False)
+        return {'status': 'error', 'message': 'Comment not found'}
 
 
-@app.route('/api/comment_d/<string:id>')
+@json_route('/api/comment_d/<string:id>')
 def delete_comment(id):
     if rds.exists(id):
         rds.delete(id)
-        return json.dumps({'status': 'ok', 'message': 'Comment deleted'}, ensure_ascii=False)
+        return {'status': 'ok', 'message': 'Comment deleted'}
     else:
-        return json.dumps({'status': 'error', 'message': 'Comment not found'}, ensure_ascii=False)
+        return {'status': 'error', 'message': 'Comment not found'}
 
 
-@app.route('/api/data/<string:key>')
+@json_route('/api/data/<string:key>')
 def get_data(key):
     if key != config["control"]["find_all_data"]:
-        return json.dumps({'status': 'error', 'message': 'wrong password'}, ensure_ascii=False)
+        return {'status': 'error', 'message': 'wrong password'}
     ret = {}
     for a in rds.keys():
         ret[a.decode()] = rds[a.decode()].decode()
-    return json.dumps({'status': 'ok', 'message': 'Data found', 'data': ret}, ensure_ascii=False)
+    return {'status': 'ok', 'message': 'Data found', 'data': ret}
 
 
-@app.route('/api/clear_db/if_you_know_what_you_are_doing/<string:password>')
+@json_route('/api/clear_db/if_you_know_what_you_are_doing/<string:password>')
 def clear_db(password):
     if password != config["control"]["clear_db"]:
-        return json.dumps({'status': 'error', 'message': 'Wrong password'}, ensure_ascii=False)
+        return {'status': 'error', 'message': 'Wrong password'}
     for a in rds.keys():
         if str(a, "utf-8").split("_")[0] == "comment":
             rds.delete(a)
 
-    return json.dumps({'status': 'ok', 'message': 'Data cleared'}, ensure_ascii=False)
+    return {'status': 'ok', 'message': 'Data cleared'}
 
 # unbanned
-@app.route('/api/unban/<string:ipk>/<string:password>')
+@json_route('/api/unban/<string:ipk>/<string:password>')
 def unban(ipk, password):
     if password != config["control"]["ban_ip"]:
-        return json.dumps({'status': 'error', 'message': 'Wrong password'}, ensure_ascii=False)
+        return {'status': 'error', 'message': 'Wrong password'}
     if rds.exists(ipk):
         rds.delete(ipk)
-        return json.dumps({'status': 'ok', 'message': 'unbanned'}, ensure_ascii=False)
+        return {'status': 'ok', 'message': 'unbanned'}
     else:
-        return json.dumps({'status': 'error', 'message': 'ip not found'}, ensure_ascii=False)
+        return {'status': 'error', 'message': 'ip not found'}
 
 
 
